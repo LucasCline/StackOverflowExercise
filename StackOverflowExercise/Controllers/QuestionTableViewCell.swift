6fd68500b3434fc1c038bd1cb951f86459fccf5d
Lucas Cline <lucaswcline@gmail.com>
2020-02-01 14:31:41 -0600

    Finished setting up the core of the UI. All pieces that I plan to display on the main view are pulling real data.

---
+++ StackOverflowExercise/Controllers/QuestionTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionTableViewCell.swift
+//  StackOverflowExercise
+//
+//  Created by Amanda Bloomer  on 2/1/20.
+//  Copyright © 2020 Lucas Cline. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+    static let identifier = "QuestionCell"
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var answerCount: UILabel!
+    @IBOutlet weak var viewsCount: UILabel!
+    @IBOutlet var collectionOfTagButtons: Array<UIButton>!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var displayName: UILabel!
+    @IBOutlet weak var score: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
--- StackOverflowExercise/Models/MainTableViewDelegate.swift
+++ StackOverflowExercise/Models/MainTableViewDelegate.swift
@@ -25,9 +25,36 @@ class MainTableViewDelegate: NSObject, UITableViewDelegate, UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = networkingManager?.questions[indexPath.row].title
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: QuestionTableViewCell.identifier, for: indexPath) as? QuestionTableViewCell else {
+            print("Failed to dequeue reusable cell with Identifier \(QuestionTableViewCell.identifier)")
+            return UITableViewCell()
+        }
+        
+        let questionForRow = networkingManager?.questions[indexPath.row]
+
+        cell.title.text = questionForRow?.title
+        cell.answerCount.text = "\(questionForRow?.answerCount ?? 0)"
+        cell.viewsCount.text = "\(questionForRow?.viewCount ?? 0) views"
+        cell.score.text = "\(questionForRow?.score ?? 0)"
+        cell.displayName.text = questionForRow?.owner?.displayName
+        
+        //set the array of tag buttons
+        for (n, button) in cell.collectionOfTagButtons.enumerated() {
+            //this is to prevent an array out of index crash for now.
+            if questionForRow?.tags.count ?? 0 >= n+1 {
+                button.setTitle(questionForRow?.tags[n], for: .normal)
+            } else {
+                button.isHidden = true
+            }
+        }
+        
+        //cell.profileImage.image = placeholder
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 175
     }
     
     private func fetchQuestions() {
